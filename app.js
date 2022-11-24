bb35356575d907c40ca67a418a19b7ee1444c949
basedest <basedscientist@gmail.com>
2022-11-25 00:35:37 +0300

    style upd

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ import express from 'express';
 const app = express();
 app.use(express.static('public'));
 
-const wsServer = new WebSocketServer({ port: 8080 });
+const wsServer = new WebSocketServer({ port: 3001 });
 
 wsServer.on('connection', (ws) => {
     ws.on('message', (message) => {
         console.log(`Received message => ${message}`);
     });
     
-    ws.send('Hello! Message From Server!!');
+    ws.send('Greetings from server!');
 
     let counter = 1;
     const spammer = setInterval(() => {
-        ws.send(`push notification #${counter++}`);
+        ws.send(`meow #${counter++}`);
     }, 1500);
 
     ws.on('close', () => {
--- public/index.js
+++ public/index.js
@@ -1,18 +1,27 @@
 let ws;
+const GREEN = '#7f7';
 
 disconnectButton.disabled = true;
 sendButton.disabled = true;
+messageInput.disabled = true;
 
-function renderMessage(message, color) {
+function renderMessage(message, connectionEvent) {
+    const timestamp = new Date().toLocaleTimeString();
     const messageElement = document.createElement('div');
-    if (color) messageElement.style.color = color;
-    messageElement.textContent = message;
+    if (connectionEvent) messageElement.style.color = GREEN;
+    messageElement.innerText = connectionEvent ? `[${timestamp}] ${message}` : message;
+    if (!connectionEvent) {
+        const spanNode = document.createElement('span');
+        spanNode.style.color = GREEN;
+        spanNode.innerText = `[${timestamp}] Received message => `;
+        messageElement.prepend(spanNode);
+    } 
     messages.appendChild(messageElement);
 }
 
 connectButton.onclick = () => {
     console.log('Connecting to server...');
-    ws = new WebSocket('ws://localhost:8080');
+    ws = new WebSocket('ws://localhost:3001');
     ws.onopen = () => {
         console.log('Connected to server');
     };
@@ -28,6 +37,7 @@ connectButton.onclick = () => {
     disconnectButton.disabled = false;
     sendButton.disabled = false;
     messageInput.disabled = false;
+    connectButton.disabled = true;
     renderMessage('Connected to server', 'gray');
 }
 
@@ -43,5 +53,6 @@ disconnectButton.onclick = () => {
     disconnectButton.disabled = true;
     sendButton.disabled = true;
     messageInput.disabled = true;
+    connectButton.disabled = false;
     renderMessage('Disconnected from server', 'gray');
 }
